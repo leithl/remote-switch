3085bb0f5531e2fccbf56aef637f376ebeaaa22e
leithl <leith.stevens@gmail.com>
2026-03-02 11:40:15 -0700

    Update test.sh
    Removed debugging and added comments

--- test.sh
+++ test.sh
@@ -25,16 +25,16 @@ echo "<head>"
 echo "<title>Airplane Hanger Heater Control</title>"
 echo "</head>"
 echo "<body>"
+
+# add some simple formatting with bootstrap
 echo "<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>"
-echo "<div class="card">"
-echo "<div class="card-header">"
-echo "<h4>Airplane Hanger Heater Control</h4>"
-echo "</div>"
 
-#echo "<p>state: $state</p>"
-#echo "<p>value: $value</p>"
-#echo "<p>gpio: $gpio_file</p>"
+echo "<div class="card">"
+
+echo "<div class="card-header">"
+  echo "<h4>Airplane Hanger Heater Control</h4>"
+echo "</div>"
 
 echo "<div class="card-body">"
 echo "<h5 class="card-title">Heater Status: "
